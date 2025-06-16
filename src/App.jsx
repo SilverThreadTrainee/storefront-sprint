d4b01dd99394e9be7d3967d098c56a2996d4a9f4
AartiPatel401 <artipatel412001@gmail.com>
2025-06-16 17:51:26 -0400

    add inventory badge

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProductCard from "./components/ProductCard";
-
+import InventoryBadge from "./components/InventoryBadge";
 function App() {
   return (
     <div className="p-6">
       <ProductCard />
+      <InventoryBadge inStock={true} />
+      <InventoryBadge inStock={false} />
     </div>
   );
 }
