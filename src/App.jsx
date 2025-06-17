f7f7166915f83ca8ddafbcd782cfa260a884edca
Dhara03042002 <joshidhara2002@gmail.com>
2025-06-17 17:21:34 -0400

    Add checkout button placeholder component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import ProductCard from "./components/ProductCard";
-
+import CheckoutButton from "./components/CheckoutButton";
 function App() {
   return (
     <div className="p-6">
       <ProductCard />
+      <CheckoutButton />
     </div>
   );
 }
---
+++ src/components/CheckoutButton.jsx
@@ -0,0 +1,7 @@
+export default function CheckoutButton() {
+  return (
+    <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
+      Proceed to Checkout
+    </button>
+  );
+}
